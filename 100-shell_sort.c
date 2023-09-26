29b17d6a391a31331c3bd2db6f95af6316468a81
Teminijesu Olaleye <holarxmanuel@gmail.com>
2023-09-25 21:35:53 -0700

    last

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -1,38 +1,40 @@
 #include "sort.h"
 
 /**
- * shell_sort - Sorts an array of integers in ascending order using Shell sort
- * @array: The array to be sorted
- * @size: The size of the array
+ * shell_sort - Sorts an array of integers in ascending order
+ * using the Shell sort algorithm with the Knuth sequence.
+ *
+ * @array: The array to be sorted.
+ * @size: The size of the array.
  */
 void shell_sort(int *array, size_t size)
 {
-    size_t gap = 1, i, j;
-    int temp;
+	size_t gap = 1, i, j;
+	int temp;
 
-    if (array == NULL || size < 2)
-        return;
+	if (array == NULL || size < 2)
+		return;
 
-    /* Calculate the initial gap value using Knuth sequence */
-    while (gap < size / 3)
-        gap = gap * 3 + 1;
+	while (gap < size / 3)
+		gap = gap * 3 + 1;
 
-    while (gap > 0)
-    {
-        for (i = gap; i < size; i++)
-        {
-            temp = array[i];
-            j = i;
-            while (j >= gap && array[j - gap] > temp)
-            {
-                array[j] = array[j - gap];
-                j -= gap;
-            }
-            array[j] = temp;
-        }
-        gap = (gap - 1) / 3;
+	while (gap > 0)
+	{
+		for (i = gap; i < size; i++)
+		{
+			temp = array[i];
+			j = i;
 
-        /* Print the array at each pass */
-        print_array(array, size);
-    }
+			while (j >= gap && array[j - gap] > temp)
+			{
+				array[j] = array[j - gap];
+				j -= gap;
+			}
+
+			array[j] = temp;
+		}
+
+		gap /= 3;
+		print_array(array, size);
+	}
 }
