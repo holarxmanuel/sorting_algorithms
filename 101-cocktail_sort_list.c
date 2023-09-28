18b288d74f6118c1dd539caa055d2f34b41b31ba
Teminijesu Olaleye <holarxmanuel@gmail.com>
2023-09-28 13:56:40 -0700

    just add it

--- 101-cocktail_sort_list.c
+++ 101-cocktail_sort_list.c
@@ -1,68 +1,92 @@
 #include "sort.h"
-#include <stdio.h>
-
-void swap_nodes(listint_t **list, listint_t *a, listint_t *b);
+#include <stdbool.h>
 
 /**
- * cocktail_sort_list - Sorts a doubly linked list of integers
- *                     using the Cocktail Shaker sort algorithm.
- * @list: A pointer to a pointer to the head of the list.
+ * swap_node_ahead - Swap a node in a listint_t doubly-linked list
+ * @list: A pointer to the head of a doubly-linked list of integers.
+ * @tail: A pointer to the tail of the doubly-linked list.
+ * @shaker: A pointer to the current swappin node
+ */
+void swap_node_ahead(listint_t **list, listint_t **tail, listint_t **shaker)
+{
+	listint_t *tmp = (*shaker)->next;
+
+	if ((*shaker)->prev != NULL)
+		(*shaker)->prev->next = tmp;
+	else
+		*list = tmp;
+	tmp->prev = (*shaker)->prev;
+	(*shaker)->next = tmp->next;
+	if (tmp->next != NULL)
+		tmp->next->prev = *shaker;
+	else
+		*tail = *shaker;
+	(*shaker)->prev = tmp;
+	tmp->next = *shaker;
+	*shaker = tmp;
+}
+
+/**
+ * swap_node_behind - Swap a node in a listint_t doubly-linked
+ * @list: A pointer to the head of a doubly-linked list of integers.
+ * @tail: A pointer to the tail of the doubly-linked list.
+ * @shaker: A pointer to the current swapping node
+ */
+void swap_node_behind(listint_t **list, listint_t **tail, listint_t **shaker)
+{
+	listint_t *tmp = (*shaker)->prev;
+
+	if ((*shaker)->next != NULL)
+		(*shaker)->next->prev = tmp;
+	else
+		*tail = tmp;
+	tmp->next = (*shaker)->next;
+	(*shaker)->prev = tmp->prev;
+	if (tmp->prev != NULL)
+		tmp->prev->next = *shaker;
+	else
+		*list = *shaker;
+	(*shaker)->next = tmp;
+	tmp->prev = *shaker;
+	*shaker = tmp;
+}
+
+/**
+ * cocktail_sort_list - Sort a listint_t doubly-linked list of integers
+ * @list: A pointer to the head of a listint_t doubly-linked list.
  */
 void cocktail_sort_list(listint_t **list)
 {
-	int swapped;
-	listint_t *current;
+	listint_t *tail, *shaker;
+	bool shaken_not_stirred = false;
 
-	if (!list || !(*list) || !(*list)->next)
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
 		return;
 
-	do {
-		swapped = 0;
-		for (current = *list; current->next != NULL; current = current->next)
+	for (tail = *list; tail->next != NULL;)
+		tail = tail->next;
+
+	while (shaken_not_stirred == false)
+	{
+		shaken_not_stirred = true;
+		for (shaker = *list; shaker != tail; shaker = shaker->next)
 		{
-			if (current->n > current->next->n)
+			if (shaker->n > shaker->next->n)
 			{
-				swap_nodes(list, current, current->next);
-				swapped = 1;
+				swap_node_ahead(list, &tail, &shaker);
+				print_list((const listint_t *)*list);
+				shaken_not_stirred = false;
 			}
 		}
-		if (!swapped)
-			break;
-		swapped = 0;
-		for (; current->prev != NULL; current = current->prev)
+		for (shaker = shaker->prev; shaker != *list;
+				shaker = shaker->prev)
 		{
-			if (current->n < current->prev->n)
+			if (shaker->n < shaker->prev->n)
 			{
-				swap_nodes(list, current->prev, current);
-				swapped = 1;
+				swap_node_behind(list, &tail, &shaker);
+				print_list((const listint_t *)*list);
+				shaken_not_stirred = false;
 			}
 		}
-	} while (swapped);
+	}
 }
-
-/**
- * swap_nodes - Swaps two nodes in a doubly linked list.
- * @list: A pointer to a pointer to the head of the list.
- * @a: The first node to swap.
- * @b: The second node to swap.
- */
-void swap_nodes(listint_t **list, listint_t *a, listint_t *b)
-{
-	listint_t *temp = NULL;
-
-	if (a->prev)
-		a->prev->next = b;
-	else
-		*list = b;
-	if (b->next)
-		b->next->prev = a;
-
-	temp = a->prev;
-	a->prev = b->prev;
-	b->prev = temp;
-
-	temp = a->next;
-	a->next = b->next;
-	b->next = temp;
-}
-
